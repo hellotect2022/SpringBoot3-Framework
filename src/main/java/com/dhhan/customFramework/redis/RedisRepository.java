f7691bdadc150dd464095050379705c9376a0af0
dhhan <dhhan@covision.co.kr>
2024-12-27 17:53:34 +0900

    redisrepository 에서 method 추가

--- src/main/java/com/dhhan/customFramework/redis/RedisRepository.java
+++ src/main/java/com/dhhan/customFramework/redis/RedisRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -59,15 +59,57 @@ public class RedisRepository {
         return Optional.empty();
     }
 
+    public <T> List<T> hmget(String key, Collection hashKeys, Class<T> classType) {
+        return (ArrayList<T>)redisTemplate.opsForHash().multiGet(key, hashKeys).stream()
+                .map(obj->JsonHelper.convertToClass((String)obj, classType))
+                .collect(Collectors.toList());
+    }
+
+
+    public void sadd(String key, Object... value){
+        redisTemplate.opsForSet().add(key, value);
+    }
+
+    public Set<Object> smembers(String key) {
+        return redisTemplate.opsForSet().members(key);
+    }
+
+    public <T> void lpush(String key, T value) {
+        redisTemplate.opsForList().leftPush(key, JsonHelper.parseObjectToJson(value));
+    }
+
+    public <T> List<T> lrange(String key, long start, long end, Class<T> classType) {
+        return redisTemplate.opsForList().range(key, start,end).stream()
+                .map(obj->JsonHelper.convertToClass((String)obj, classType))
+                .collect(Collectors.toList());
+    }
+
+    public <T> Optional<T> lpop(String key, Class<T> classType) {
+        String jsonData = (String) redisTemplate.opsForList().leftPop(key);
+        if (StringUtils.hasText(jsonData)) {
+            return Optional.ofNullable(JsonHelper.convertToClass(jsonData,classType));
+        }
+        return Optional.empty();
+    }
+
+    public void rpush(String key, Object value) {
+        redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    // Sorted SET 저장
+    public void zadd(String key, Object value, double score) {
+        redisTemplate.opsForZSet().add(key, JsonHelper.parseObjectToJson(value), score);
+    }
+
+    public Set zrange(String key, long start, long end) {
+        return redisTemplate.opsForZSet().rangeWithScores(key,start, end);
+    }
+
+    // basic String
     public Optional<String> hget(String key, String hashKey) {
         return hget(key, hashKey, String.class);
     }
     public Optional<String> get(String key) {
         return get(key, String.class);
     }
-
-    public void sadd(String key, Object... value){
-        redisTemplate.opsForSet().add(key, value);
-    }
-
 }
